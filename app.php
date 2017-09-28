b63369849dad3d335e60cd016b04e15d3b9a3c2b
liubang <it.liubang@gmail.com>
2017-09-28 22:16:33 +0800

    +

--- app.php
+++ app.php
@@ -1,12 +1,21 @@
 <?php
 
-include __DIR__ . '/app/module/index/controller/Index.php';
+error_reporting(E_ALL);
 
-$app = new linger\framework\Application([
-    'name' => 'test'
-]);
 
-var_dump($app);
-//$app->getRequest()->setUri('/index/index/index');
-$app->getRequest()->setUri('/index/index/index/aaa/bbb');
-$app->run();
+try {
+
+	//include __DIR__ . '/app/module/index/controller/Index.php';
+
+	$app = new linger\framework\Application([
+    		'name' => 'test'
+	]);
+
+	//var_dump($app);
+	//$app->getRequest()->setUri('/index/index/index');
+	$app->getRequest()->setUri('/index/index/index/aaa/bbb');
+	$app->run();
+} catch (Exception $e) {
+	echo $e->getMessage(), PHP_EOL;
+	echo $e->getTraceAsString(), PHP_EOL;
+}
